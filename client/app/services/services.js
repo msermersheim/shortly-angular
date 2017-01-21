f129e9e29b11bf33fd33d89871dee974dbf7282a
Michael Sermersheim <m.sermersheim@gmail.com>
2017-01-20 20:27:43 -0800

    first few tests passing

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,23 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  return {
+    getAll: function () {
+      return $http({
+        method: 'GET',
+        url: '/api/links'
+      })
+    },
+    addOne: function (data) {
+      return $http({
+        method: 'POST',
+        url: '/api/links',
+        data: data
+      })
+    }
+  }
 })
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
